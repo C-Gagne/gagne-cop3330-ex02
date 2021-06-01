786d879b48e5143e2c8b7ebcd5963d8a12bfb7c4
C.Gagne <crystal.r.gagne@gmail.com>
2021-06-01 10:11:05 -0400

    Initial commit

---
+++ src/main/java/org/example/ex2/App.java
@@ -0,0 +1,25 @@
+package org.example.ex2;
+
+import java.util.Scanner;
+
+public class App
+{
+    public static void main(String[] args)
+    {
+        String str;
+        System.out.println("What is the input string?");
+        str = getString();
+        System.out.println(charNum(str));
+    }
+
+    public static String getString()
+    {
+        Scanner newStr = new Scanner(System.in);
+        return newStr.nextLine();
+    }
+
+    public static Integer charNum(String str)
+    {
+        return str.length(); // Equivalent to: Integer strLength = str.length(); return strLength;
+    }
+}
